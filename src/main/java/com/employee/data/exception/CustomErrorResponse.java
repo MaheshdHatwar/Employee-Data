3a18df824b043feed10722ba1641d83dcce359c0
Mahesh Hatwar <mahesh.hatwar@mindtree.com>
2022-07-27 00:46:15 +0530

    third commit

--- src/main/java/com/employee/data/exception/CustomErrorResponse.java
+++ src/main/java/com/employee/data/exception/CustomErrorResponse.java
@@ -1,12 +1,12 @@
 package com.employee.data.exception;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Data
-//@AllArgsConstructor
-//@NoArgsConstructor
+
+
 public class CustomErrorResponse {
 
 	private int statusCode;
